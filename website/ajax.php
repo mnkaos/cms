75aa06ae018b381a401d74c865c2e398506cd876
Minoru Nomura <kaos_93@hotmail.com>
2014-02-27 20:47:58 -0800

    Prueba de cambio.

--- website/ajax.php
+++ website/ajax.php
@@ -8,7 +8,7 @@ $postnumbers = is_numeric($_POST['number']) ? $_POST['number'] : die();
 $run = mysql_query("SELECT * FROM foro ORDER BY fecha DESC LIMIT ".$postnumbers." OFFSET ".$offset);
 
 
-while($row = mysql_fetch_array($run)) {
+while($row = mysql_fetch_array($run)) :
 	
 	$content = substr(strip_tags($row['mensaje']), 0, 450);
 	
@@ -18,6 +18,6 @@ while($row = mysql_fetch_array($run)) {
 	echo '<p>'.$content.'...</p>'."\n";
 	echo '<hr />'."\n";
 
-}
+endwhile;
 
 ?>
